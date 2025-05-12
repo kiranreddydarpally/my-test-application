0962e21d91c9ba9b0b4f3b63c6441ff3c851d7eb
kiranreddydarpally <darpallykiranreddy@gmail.com>
2025-05-12 18:43:50 +0530

    add post button clicable

--- src/App.tsx
+++ src/App.tsx
@@ -1,99 +1,17 @@
-import React from "react";
 import "./App.css";
-import Cats from "./components/cats";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/home/home";
+import AddPost from "./components/add-post/add-post";
 
 function App() {
   return (
-    <div className="app">
-      <header className="header">
-        <div>
-          <img src="/ZAANVAR_FINAL LOGO 2.svg" alt="ZAANVAR LOGO" />
-        </div>
-        <div className="menu-items">
-          <div>Home</div>
-          <div>Services</div>
-          <div>Book a Vet</div>
-          <div>Shopping</div>
-          <div>About us</div>
-        </div>
-        <div>
-          <button className="login-btn">LOG IN</button>
-        </div>
-      </header>
-      <main className="path">
-        Adoption / Home / <span className="my-posts">My posts </span>
-      </main>
-      <main className="main-content">
-        <div className="buttons">
-          <div className="two-buttons">
-            <div>
-              <button className="adopt-btn">Adopt</button>
-            </div>
-            <div>
-              <button className="rehomed-btn">Rehomed</button>
-            </div>
-          </div>
-          <div>
-            <button className="add-post">+ Add post</button>
-          </div>
-        </div>
-        <div className="cats">
-          <Cats
-            src={"/Mask group.svg"}
-            title={"Rottweiler"}
-            age={"2 Years 8 Months,"}
-            area={"Female Ameerpet, Hyderabad (75 Miles)"}
-          />
-          <Cats
-            src={"/Mask group (1).svg"}
-            title={"Rottweiler"}
-            age={"2 Years 8 Months,"}
-            area={"Female Ameerpet, Hyderabad (75 Miles)"}
-          />
-          <Cats
-            src={"/Mask group (2).svg"}
-            title={"Rottweiler"}
-            age={"2 Years 8 Months,"}
-            area={"Female Ameerpet, Hyderabad (75 Miles)"}
-          />
-        </div>
-        <div>Matched Profiles</div>
-        <div className="table">
-          <table style={{ width: "100%" }}>
-            <tr>
-              <th>Full Name</th>
-              <th>Pet Name</th>
-              <th>Location</th>
-              <th>Status</th>
-            </tr>
-            <tr>
-              <td>Ranjith Soma</td>
-              <td>Simba</td>
-              <td>Yellareddy road Ameerpet, Hyderabad</td>
-              <td>80%</td>
-            </tr>
-            <tr>
-              <td>Ranjith Soma</td>
-              <td>Simba</td>
-              <td>Yellareddy road Ameerpet, Hyderabad</td>
-              <td>80%</td>
-            </tr>
-            <tr>
-              <td>Ranjith Soma</td>
-              <td>Simba</td>
-              <td>Yellareddy road Ameerpet, Hyderabad</td>
-              <td>80%</td>
-            </tr>
-            <tr>
-              <td>Ranjith Soma</td>
-              <td>Simba</td>
-              <td>Yellareddy road Ameerpet, Hyderabad</td>
-              <td>80%</td>
-            </tr>
-          </table>
-        </div>
-      </main>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/add-post" element={<AddPost />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/add-post/add-post.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddPost = () => {
+  return <div>add</div>;
+};
+
+export default AddPost;
